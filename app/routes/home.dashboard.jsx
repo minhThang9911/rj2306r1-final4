94c0a95361defa4545a839d8b2e4dce5c4ee4a62
minhthang <minhthang0792@gmail.com>
2023-08-28 22:06:07 +0700

    update

--- app/routes/home.dashboard.jsx
+++ app/routes/home.dashboard.jsx
@@ -1,5 +1,29 @@
+import { useActionData } from "@remix-run/react";
+import { uploadImg } from "~/server/upload.server";
+
 function DashboardPage() {
-	return <div>DashBoard</div>;
+	const res = useActionData();
+	console.log(res);
+	return (
+		<div>
+			<form method="post">
+				<button type="submit">Test Upload</button>
+			</form>
+			<input
+				type="file"
+				value="upload"
+				onChange={(e) => {
+					console.log(e.target);
+				}}
+			/>
+		</div>
+	);
 }
 
+export const action = async ({ request, params }) => {
+	const res = await uploadImg(
+		"https://fonesmart.com.vn/upload_images/images/2023/03/11/hinh-nen-phi-hanh-gia-01.jpg"
+	);
+	return { res };
+};
 export default DashboardPage;
